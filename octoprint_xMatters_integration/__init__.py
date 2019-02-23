46789de00cb07a6409a176a4223504085e26ad3c
Vadym <vadim.svv@gmail.com>
2019-02-23 15:34:38 -0500

    changed logger level

--- octoprint_xMatters_integration/__init__.py
+++ octoprint_xMatters_integration/__init__.py
@@ -45,47 +45,50 @@ class XmattersIntegrationPlugin(octoprint.plugin.StartupPlugin,
 		payload = "{ \"properties\": { \"Title\": \"%s\", \"Details\": \"%s\"}, \"recipients\": [ \"%s\" ] }" % (
 			title, details, recipients)
 
-		self._logger.info(" sending notification with payload: %s" % payload)
+		self._logger.debug(" sending notification with payload: %s" % payload)
 		r = requests.post(url=url, auth=(apikey, secret), data=payload, headers=headers)
 
-		self._logger.info(" response: %s" % r.text)
+		self._logger.debug(" response: %s" % r.text)
 
 	# http:/docs.octoprint.org/en/master/events/index.html#printing
 	def on_event(self, event, payload):
 		title = "OctoPrint "
-		
+
+		if not self._settings.get(["enabled"]):
+			return
+
 		if event == "PrintStarted" and self._settings.get(["enablePrintStarted"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Printing started for the file: %s ,<br> storage location %s" % (payload["path"], payload["origin"])
 			title += " Print Started %s" % payload["name"]
 			self.send_xmatters_notification(title, details)
 
 		if event == "PrintDone" and self._settings.get(["enablePrintDone"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Printing Finished for the file: %s ,<br> storage location %s <br> time: %s " % (payload["path"], payload["origin"], payload["time"])
 			title += " Printing Finished %s " % payload["name"]
 			self.send_xmatters_notification(title, details)
 
 		if event == "PrintPaused" and self._settings.get(["enablePrintPaused"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Printing Paused for the file: %s ,<br> storage location %s, <br> position: %s" % (payload["path"], payload["origin"], payload["position"])
 			title += " Printing Paused %s " % payload["name"]
 			self.send_xmatters_notification(title, details)
 
 		if event == "PrintResumed" and self._settings.get(["enablePrintResumed"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Printing Resumed for the file: %s%s ,<br> storage location %s" % (payload["path"], payload["name"], payload["origin"])
 			title += " Printing Resumed %s " % payload["name"]
 			self.send_xmatters_notification(title, details)
 
 		if event == "PrintFailed" and self._settings.get(["enablePrintFailed"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Printing Failed for the file: %s ,<br> storage location %s,<br> time: %s,<br> failure reason: %s " % (payload["path"], payload["origin"], payload["time"], payload["reason"])
 			title += " Printing Failed %s " % payload["name"]
 			self.send_xmatters_notification(title, details)
 
 		if event == "MovieDone" and self._settings.get(["enableMovieDone"]):
-			self._logger.info("xMatters ( event: %s)" % event)
+			self._logger.debug("xMatters ( event: %s)" % event)
 			details = "Rendering Time-laps is finished. File: %s ,<br> GCODE file: %s" % (payload["movie"], payload["gcode"])
 			title += " Time-laps rendered %s " % payload["movie_basename"]
 			self.send_xmatters_notification(title, details)
